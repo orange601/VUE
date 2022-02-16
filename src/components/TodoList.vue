2d0c197f9b5463640dd30e96bc3132473bb70bbd
orange <oltop@naver.com>
2022-02-16 09:00:37 +0900

    Add files via upload

---
+++ src/components/TodoList.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    created: function(){
+        console.log('test');
+    }
+}
+</script>
+
+<style>
+li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.removeBtn {
+    margin-left: auto;
+    color: #de4343;
+}
+.checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+} 
+.textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+}
+.checkBtnCompleted {
+    color: #b3adad;
+}
+
+/* list item transition effect */
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
